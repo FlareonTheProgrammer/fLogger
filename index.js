0d2069cc7afa7f4de557beb06f380abb10f18a48
FlareonTheProgrammer <theinnovator64@gmail.com>
2020-06-23 02:19:22 -0500

    non-breaking changes

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 const fmt = require("./fmt");
 
-const core = (title, color, msg) => {
-    return console.log(`${fmt.reset}[ ${fmt.time} ] [${fmt.txt[color] + title + fmt.reset}] » ${fmt.reset + msg + fmt.reset}`)
+const core = (method, title, color, msg) => {
+    if (msg === undefined && color !== undefined) {
+        msg = color;
+        color = title;
+        title = method;
+        method = "log";
+    };
+    return console[method](`${fmt.reset}[${fmt.time}] [${fmt.txt[color] + title + fmt.reset}] » ${fmt.reset + msg + fmt.reset}`)
 }
 
 const info = (msg) => {
-    return core("INFO", "blue", msg)
+    return core("info", "INFO", "blue", msg)
 }
 
 const log = (msg) => {
-    return core("LOG", "lGreen", msg)
+    return core("log", "LOG", "lGreen", msg)
 }
 
 const warn = (msg) => {
-    return core("WARN", "yellow", msg)
+    return core("warn", "WARN", "yellow", msg)
 }
 
 const error = (msg) => {
-    return core("ERROR", "red", msg)
+    return core("error", "ERROR", "red", msg)
 }
 
-/* This was from testing
-core("Custom", "lCyan", "This is a custom thing.");
+// This was from testing
+core("info", "Custom", "lCyan", "This is a custom thing.");
+core("Custom", "lBlue", "This is a custom thing that defaults to using console.log");
 info("This is info.");
 log("This is a log.");
 warn("This is a warning.")
 error("This is an error.");
-*/
+
 
 module.exports = {
     custom: core,
